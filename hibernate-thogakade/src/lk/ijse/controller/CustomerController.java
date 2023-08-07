2a2d73754da5ca4bc465cab691f54e154d0494f0
Dinarbrooke <bethmij@gmail.com>
2023-08-07 13:00:35 +0530

    modify customer Form

--- hibernate-thogakade/src/lk/ijse/controller/CustomerController.java
+++ hibernate-thogakade/src/lk/ijse/controller/CustomerController.java
@@ -1,4 +1,30 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class CustomerController {
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtContact;
+    public TableColumn ColId;
+    public TableColumn ColName;
+    public TableColumn ColAddress;
+    public TableColumn ColContact;
+    public TableColumn ColAction;
+
+    public void navigateToHome(MouseEvent mouseEvent) {
+    }
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnClearOnAction(ActionEvent actionEvent) {
+    }
 }

96112f35305b9476d4686eb9584dc0c1b2c3d4c0
Dinarbrooke <bethmij@gmail.com>
2023-08-07 12:37:21 +0530

    added main form

--- hibernate-thogakade/src/lk/ijse/controller/MainController.java
+++ hibernate-thogakade/src/lk/ijse/controller/MainController.java
@@ -1,4 +1,15 @@
 package lk.ijse.controller;
 
+import javafx.scene.input.MouseEvent;
+
 public class MainController {
+
+    public void customerOnClick(MouseEvent mouseEvent) {
+    }
+
+    public void itemOnClick(MouseEvent mouseEvent) {
+    }
+
+    public void orderOnClick(MouseEvent mouseEvent) {
+    }
 }

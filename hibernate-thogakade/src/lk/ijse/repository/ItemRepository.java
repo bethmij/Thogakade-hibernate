2681b9cea992c43958664831ed349575bca332fd
Dinarbrooke <bethmij@gmail.com>
2023-08-08 09:19:10 +0530

    added item Repository

---
+++ hibernate-thogakade/src/lk/ijse/repository/ItemRepository.java
@@ -0,0 +1,126 @@
+package lk.ijse.repository;
+
+import javafx.scene.control.Alert;
+import lk.ijse.config.SessionFactoryConfig;
+import lk.ijse.entity.Item;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
+public class ItemRepository {
+
+        private Session session;
+
+        public ItemRepository(){
+            session = SessionFactoryConfig.getInstance().getSession();
+        }
+
+        public int saveItem(Item item){
+            Transaction transaction = session.beginTransaction();
+            try {
+                int itemId = (int) session.save(item);
+                transaction.commit();
+                return itemId;
+
+            }catch (Exception e){
+                transaction.rollback();
+                new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+                return -1;
+            }finally {
+                session.close();
+            }
+        }
+
+        public boolean updateItem(Item item){
+            Transaction transaction = session.beginTransaction();
+            try {
+                session.update(item);
+                transaction.commit();
+                return true;
+
+            }catch (Exception e){
+                transaction.rollback();
+                new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+                return false;
+            }finally {
+                session.close();
+            }
+        }
+
+        public boolean deleteItem(Item item){
+            Transaction transaction = session.beginTransaction();
+            try {
+                session.delete(item);
+                transaction.commit();
+                return true;
+
+            }catch (Exception e){
+                transaction.rollback();
+                new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+                return false;
+            }finally {
+                session.close();
+            }
+        }
+
+        public Item getItem(int id){
+            Transaction transaction = session.beginTransaction();
+            Item item = null;
+
+            try {
+                item = session.get(Item.class, id);
+                transaction.commit();
+
+            }catch (Exception e){
+                transaction.rollback();
+                new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+            }finally {
+                session.close();
+            }
+
+            return item;
+        }
+
+        public List<Item> getAllItem(){
+            Transaction transaction = session.beginTransaction();
+            List<Item> itemList = null;
+
+            try {
+                itemList = session.createSQLQuery("SELECT * FROM item").addEntity(Item.class).list();
+                transaction.commit();
+
+            }catch (Exception e){
+                transaction.rollback();
+                new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+
+            }finally {
+                session.close();
+            }
+            return itemList;
+        }
+
+        public int getItemCode(){
+            Transaction transaction = session.beginTransaction();
+            List<Integer> id = null;
+
+            try {
+                id = session.createSQLQuery("SELECT item_code FROM item ORDER BY item_code DESC LIMIT 1").list();
+                transaction.commit();
+
+            }catch (Exception e){
+                transaction.rollback();
+                new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+
+            }finally {
+                session.close();
+            }
+
+            if(!id.equals(null))
+                return id.get(0);
+            else
+                return 0;
+        }
+
+
+}
